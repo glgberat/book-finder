183cca2c8ac26390158becc3167ba8110a0fbd64
Abbush <glgberat@yahoo.com>
2022-05-16 02:20:18 -0400

    heroku

---
+++ client/build/precache-manifest.4699e95f82632ef118f6e12d84361b75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b685590e2b7023a394161f8dcc64726c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59790736bf0d46cb31f4",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "84f2f700b0ee34d2fd72",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "59790736bf0d46cb31f4",
+    "url": "/static/js/2.9902e264.chunk.js"
+  },
+  {
+    "revision": "98dfceff0abe42054b0ddb9cecd3f331",
+    "url": "/static/js/2.9902e264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84f2f700b0ee34d2fd72",
+    "url": "/static/js/main.b65fd172.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
