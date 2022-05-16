6c6ab5aaaf3cc1b7767dcbac0c89701ac8137035
Abbush <glgberat@yahoo.com>
2022-05-15 20:28:31 -0400

    resolver,Schemas

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const {ApolloServer} = require ('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-
+const {authMiddleware} = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const {typeDefs, resolvers} = require('./schemas');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,6 +15,19 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer()
 
 app.use(routes);
 
